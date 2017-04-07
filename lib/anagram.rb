a4f94884c28e8983b47895fda689b14f21525dd4
github username <github email address>
2017-04-07 15:59:24 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :words
+
+
+    def initialize(word)
+        @word = word.split("").sort!
+    end
+
+    def match(things)
+        final_array = []
+
+        things.each do |thing|
+            if thing.split("").sort! == @word
+                final_array << thing
+            end
+        end
+
+        final_array
+
+    end
+end
